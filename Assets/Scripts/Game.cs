2d2f12f274ef4d02b169bb80b546c9a6979299ed
Advejo <advejoJeb@gmail.com>
2022-06-08 22:29:04 +0500

    TextButon

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,32 +6,40 @@ using UnityEngine.UI;
 
 public class  Game : MonoBehaviour
 {
-    int correctAnswer ;
-    
-    public int[] array = new int[] { 4, 16, 3, 8 };
+	
+    int correctAnswer = 0  ;
+    int[] example = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int[] showEx = new int[] {0,0,0,0};
     public Text[] buttonText;
-    public Text[] text;
+    public Text placeholder;
+    public Text status;
+    public Text textError;
+
     public int error = 0;
 
     public void showForm()
     {
-        for(int i=0; i<array.Length && i<buttonText.Length; i++)
+        for(int i=0; i<showEx.Length && i<buttonText.Length; i++)
         {
-            buttonText[i].text = array[i].ToString(); 
+            buttonText[i].text = showEx[i].ToString(); 
         }
     }
     
-    
     public void NextQuestion()
     {
-        correctAnswer = Random.Range(0, array.Length ); 
-        text[0].text =  array[correctAnswer].ToString(); 
+        for(int i=0; i<showEx.Length && i<buttonText.Length; i++)
+        {
+            showEx[i] = example[Random.Range(0, example.Length )];
+            correctAnswer += 1 ;
+        }
+        
+        correctAnswer = Random.Range(0, showEx.Length ); 
+        placeholder.text =  showEx[correctAnswer].ToString(); 
         
  
     }
     void Start()
     {
-        
         NextQuestion();
     }
     
@@ -51,14 +59,14 @@ public class  Game : MonoBehaviour
     {
         if(index == correctAnswer )
         {
-            text[1].text = "<color=green>Good</color>";
+            status.text = "<color=green>Good</color>";
             NextQuestion();
             
         }else
         {
             error += 1;
-            text[1].text = "<color=red>wrong</color>";
-            text[2].text = error.ToString();
+            status.text = "<color=red>wrong</color>";
+            textError.text = error.ToString();
             if(error == 5)
             {
                 OnWasted();
